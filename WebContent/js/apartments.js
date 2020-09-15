98ea522cf18f81e7b1820a47b336d4bb81b97459
milanovicn <63123416+milanovicn@users.noreply.github.com>
2020-09-15 21:15:15 +0200

    Minor changes in search algorithm

--- WebContent/js/apartments.js
+++ WebContent/js/apartments.js
@@ -61,6 +61,7 @@ $(document).ready(function () {
 	$("#showSearchDiv").click(function () {
 		$("#searchDiv").toggle();
 	});
+	
 	$("#searchForm").submit(function (event) {
 		event.preventDefault();
 		searchApartments();
